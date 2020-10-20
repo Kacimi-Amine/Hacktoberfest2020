fbb248adde7d09e8a72b60d86c81c5e304d40f79
Chamali <basnayakechamali@gmai.com>
2020-10-19 18:38:38 -0700

    Quick Sort

--- Dart/quickSort.dart
+++ Dart/quickSort.dart
@@ -1,27 +1,44 @@
-int partition(List list, int low, int high) {
-  if (list == null || list.length == 0) return 0;
-  int pivot = list[high];
-  int i = low - 1;
+void main() {
+  List<int> list = [8, 9, 1, 3, 4, 6, 5, 2];
+  int high = list.length - 1;
+  int low = 0;
+  List<int> result = quickSort(list, low, high);
+  print(result);
+}
 
+List<int> quickSort(List list, int low, int high) {
+  if (low < high) {
+    int pi = partition(list, low, high);
+    quickSort(list, low, pi - 1);
+    quickSort(list, pi + 1, high);
+  }
+  return list;
+}
+
+int partition(List<int> list, low, high) {
+  // Base check
+  if (list.isEmpty) {
+    return 0;
+  }
+  // Take our last element as pivot and counter i one less than low
+  int pivot = list[high];
+
+  int i = low - 1;
   for (int j = low; j < high; j++) {
-    if (list[j] <= pivot) {
+    // When j is < than pivot element we increment i and swap arr[i] and arr[j]
+    if (list[j] < pivot) {
       i++;
       swap(list, i, j);
     }
-  swap(list, i+1, high);
-  return i+1;
+  }
+  // Swap the last element and place in front of the i'th element
+  swap(list, i + 1, high);
+  return i + 1;
 }
 
+// Swapping using a temp variable
 void swap(List list, int i, int j) {
   int temp = list[i];
   list[i] = list[j];
   list[j] = temp;
-}
-
-void quickSort(List list, int low, int high) {
-  if (low < high) {
-    int pi = partition(list, low, high);
-    quickSort(list, low, pi-1);
-    quickSort(list, pi+1, high);
-  }
 }
